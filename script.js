31eee4c963ee0a5069a6de9e4a8945b1b5991edb
Nishita-codes <nishitapatidar03@gmail.com>
2024-12-31 19:51:25 +0530

    added files

---
+++ script.js
@@ -0,0 +1,4 @@
+var audio = document.createElement('audio');
+audio.setAttribute('src', 'img-audio/sound.mp3');
+audio.loop=true;
+// audio.play(); //command off-audio will stop , command on-audio will play
